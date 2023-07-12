b5289e9d6b35ea54967747584362be969d7a2d24
André Engelbrecht <35694726+andre-engelbrecht@users.noreply.github.com>
2023-07-12 13:38:13 -0500

    Add Data project and attempt to run migrations during Docker build

---
+++ Sample_API/Sample_API.Data/Entities/Model1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_API.Data.Entities
+{
+    public class Model1
+    {
+        public Guid Id { get; set; }
+        public string? Value { get; set; }
+    }
+}
---
+++ Sample_API/Sample_API.Data/SampleDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample_API.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_API.Data
+{
+    public class SampleDBContext : DbContext
+    {
+        public SampleDBContext(DbContextOptions<SampleDBContext> options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Model1> Model1 { get; set; }
+    }
+}
