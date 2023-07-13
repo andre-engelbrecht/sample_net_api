691bab13605422ad49d8b85a29cadbc33721492a
André Engelbrecht <35694726+andre-engelbrecht@users.noreply.github.com>
2023-07-13 14:54:10 -0500

    Create DbTestController.cs

---
+++ Sample_API/Sample_API.API/Controllers/DbTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sample_API.Data;
+
+namespace Sample_API.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DbTestController : ControllerBase
+    {
+        private SampleDBContext context;
+
+        public DbTestController(SampleDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet(Name = "TestReadFromDB")]
+        public int TestReadFromDB()
+        {
+            return context.Model1.Count();
+        }
+    }
+}
